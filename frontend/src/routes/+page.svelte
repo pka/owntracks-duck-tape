98e7c5a9895230ea9708e7d1140ac445f1522085
Pirmin Kalberer <pirmin.kalberer@gmail.com>
2025-02-23 22:08:26 +0100

    Add tracking layer

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,17 +3,28 @@
         MapLibre,
         NavigationControl,
         ScaleControl,
+        GeoJSONSource,
+        LineLayer,
     } from "svelte-maplibre-gl";
+    let date = $state(new Date().toISOString().split("T")[0]);
 </script>
 
 <MapLibre
     class="map"
     style="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
-    zoom={8}
+    zoom={9}
     center={{ lng: 9.437489, lat: 47.050207 }}
 >
     <NavigationControl />
     <ScaleControl />
+    <GeoJSONSource data={`http://127.0.0.1:8083/tracks?date=${date}`}>
+        <LineLayer
+            paint={{
+                "line-color": "#ff0000",
+                "line-width": 4,
+            }}
+        />
+    </GeoJSONSource>
 </MapLibre>
 
 <style>
