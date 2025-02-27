c39b52d8ac3e1dd3d34264eced4441fcdafc7874
Pirmin Kalberer <pirmin.kalberer@gmail.com>
2025-02-27 21:16:40 +0100

    Extract Map component

---
+++ frontend/src/routes/Map.svelte
@@ -0,0 +1,35 @@
+<script>
+    import {
+        MapLibre,
+        NavigationControl,
+        ScaleControl,
+        GeoJSONSource,
+        LineLayer,
+    } from "svelte-maplibre-gl";
+    let { date } = $props();
+</script>
+
+<MapLibre
+    class="map"
+    style="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
+    zoom={9}
+    center={{ lng: 9.437489, lat: 47.050207 }}
+>
+    <NavigationControl />
+    <ScaleControl />
+    <GeoJSONSource data={`http://127.0.0.1:8083/tracks?date=${date}`}>
+        <LineLayer
+            paint={{
+                "line-color": "#ff0000",
+                "line-width": 4,
+            }}
+        />
+    </GeoJSONSource>
+</MapLibre>
+
+<style>
+    :global(.map) {
+        width: 100%;
+        height: 400px;
+    }
+</style>
